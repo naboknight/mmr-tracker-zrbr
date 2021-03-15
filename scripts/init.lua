92150ffe66c351d97cbe49558a97f123fafe20a8
naboknight <naboknight@gmail.com>
2021-03-15 12:25:30 -0300

    15/03/2021

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,5 +1,6 @@
 Tracker:AddItems("items/dungeon_items.json")
 Tracker:AddItems("items/options.json")
+Tracker:AddItems("items/keaton_items.json")
 
 ScriptHost:LoadScript("scripts/logic.lua")
 
@@ -8,6 +9,8 @@ Tracker:AddMaps("maps/maps.json")
 Tracker:AddLocations("locations/overworld.json")
 Tracker:AddLocations("locations/dungeons.json")
 
+Tracker:AddLayouts("layouts/keaton.json")
+Tracker:AddLayouts("layouts/options.json")
 Tracker:AddLayouts("layouts/shared.json")
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
